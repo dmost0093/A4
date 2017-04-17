be31cc21661832296d9c67984196635a3050245c
dmost0093 <sangilhan93@hotmail.com>
2017-04-17 13:16:46 -0500

    Prototypes(regions.c)
    define prototypes for the regions.c.

--- regions.c
+++ regions.c
@@ -37,7 +37,14 @@ struct REGION
 //-------------------------------------------------------------------------------------
 // PROTOTYPES
 //-------------------------------------------------------------------------------------
-
+Boolean rinit(const char *region_name, r_size_t region_size);
+Boolean rchoose(const char *region_name);
+const char *rchosen();
+void *ralloc(r_size_t block_size);
+r_size_t rsize(void *block_ptr);
+Boolean rfree(void *block_ptr);
+void rdestroy(const char *region_name);
+void rdump(void);
 //-------------------------------------------------------------------------------------
 // FUNCTIONS
 //-------------------------------------------------------------------------------------
