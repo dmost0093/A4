26a81889e81d79279bfcadefa50dcaea22dadbe4
dmost0093 <sangilhan93@hotmail.com>
2017-04-19 10:37:52 -0500

    Edit rinit function && struct for node
    chang the order of the code and edit the code.
    Change the placement of the struct of Node

--- regions.c
+++ regions.c
@@ -9,6 +9,7 @@
 //
 //-----------------------------------------
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 //-------------------------------------------------------------------------------------
@@ -16,24 +17,23 @@
 //-------------------------------------------------------------------------------------
 typedef enum BOOL{false, true} Boolean;
 typedef unsigned short r_size_t;
-typedef struct NODE node;
 typedef struct REGION Region;
-struct NODE
-{
-    Region data;//data that stored in the node
-    struct Node * next;//next Node
-}
 struct REGION
 {
     const char *name;//name of the memory region
     r_size_t size;//maximum memory that can be store
     r_size_t occupied;//total memory that been occupied
     char **memory;//momory of the region store
-}
+};
+struct Node 
+{
+   Region data;//data that stored in the node
+   struct Node *next;
+};
 //-------------------------------------------------------------------------------------
 // VARIABLES
 //-------------------------------------------------------------------------------------
-static struct Node *curr = NULL;//currently chosen region
+static struct Node *currNode = NULL;//currently chosen region
 static struct Node *head = NULL;//head of the region list
 //-------------------------------------------------------------------------------------
 // PROTOTYPES
@@ -53,40 +53,59 @@ void rdump(void);
 //Create and initialize a region with the given name and the given size
 Boolean rinit(const char * region_name, r_size_t region_size)
 {
-    Boolean result;//return value
-    //check the name of the region is already used or not
-    struct Node *curr = head;
-    Region *check = &curr->data;//datat store in the current node
-    if(curr == NULL)
-    {//list is empty
+    Boolean result = false; //return value
+    //check the name of the region is already used or not    
+    if (head == NULL)
+    { //case 0 number of region.
         result = true;
+        //create new region
+        char newMemory[1][region_size];
+        Region newRegion;
+        newRegion.name = region_name;
+        newRegion.size = region_size;
+        newRegion.occupied = 0;
+        newRegion.memory = newMemory;
+        //create new node
+        struct Node *newNode = (struct Node*) malloc(sizeof(struct Node));
+        newNode->data = newRegion;
+        newNode->next = head;
+        head = newNode;
+        currNode = newNode;
+        numOfr++;
+        return result;
     }
     else
-    {
-        while(curr->next != NULL)
-        {
-            if(check->name == region_name)
-            {//case the region name used already.
-                printf("The region name %s is already used", region_name);
-                result = false;
-                return result;//return false
+    {//case there are more than 0 number of region
+        struct Node *curr = head;
+        Region check = curr->data; //data store in the current node
+        while (curr->next != NULL)
+        {//check the name of region is unique
+            if (check.name == region_name)
+            { //case the region name used already.
+                printf("The region name (%s) has been already used\n", region_name);
+                return result; //return false
             }
             //update the current node and data stored in node
             curr = curr->next;
-            check = &curr->data;
+            check = curr->data;
         }
+        
+        //create new region
+        char newMemory[1][region_size];
+        Region newRegion;
+        newRegion.name = region_name;
+        newRegion.size = region_size;
+        newRegion.occupied = 0;
+        newRegion.memory = newMemory;
+        //create new node
+        struct Node *newNode = (struct Node*) malloc(sizeof(struct Node));
+        newNode->data = newRegion;
+        newNode->next = head;
+        head = newNode;
+        currNode = newNode;
+        numOfr++;
+        printList();
+        result = true;
+        return result;
     }
-    //create new region
-    Region newRegion;
-    newRegion.name = region_name;
-    newRegion.size = region_size;
-    newRegion.occupied = 0;
-
-    //create new node
-    struct Node *newNode = (struct Node) malloc(sizeof(struct Node));
-    newNode.data = newRegion;
-    newNode.next = head;
-    head = newNode;
-    result = true;
-    return result;
 }
